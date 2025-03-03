8f5f698cf91cc7d8b5edd9039708c141b6fe786a
Subha <msubhadeep9635@gmail.com>
2025-03-03 17:56:41 +0530

    interview feedback page done

--- app/dashboard/interview/[interviewId]/feedback/page.jsx
+++ app/dashboard/interview/[interviewId]/feedback/page.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
+import { useParams, useRouter } from 'next/navigation'
+import { db } from '../../../../../utils/db'
+import { UserAnswer } from '../../../../../utils/schema'
+import { eq } from 'drizzle-orm'
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "../../../../../@/components/components/ui/collapsible"
+import { ChevronsUpDown } from 'lucide-react'
+import { Button } from '../../../../../@/components/components/ui/button'
 
 function Feedback() {
+  const params = useParams();
+  const [feedbackList, setFeedbackList] = useState([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (params.interviewId) {
+      getFeedback();
+    }
+  }, [params.interviewId]);
+
+  const getFeedback = async () => {
+    const result = await db
+      .select()
+      .from(UserAnswer)
+      .where(eq(UserAnswer.mockIdRef, params.interviewId))
+      .orderBy(UserAnswer.id);
+
+    console.log(result);
+    setFeedbackList(result);
+  };
+
   return (
-    <div>Feedback</div>
-  )
+    <div className='p-10'>
+      <h2 className='font-bold text-2xl '><strong className='text-green-500'>Congratulations!</strong> Here is your interview feedback</h2>
+      <h2 className='text-primary text-lg my-3'>
+        Your overall interview rating: <strong>7/10</strong>
+      </h2>
+      <h2 className='text-sm text-gray-400'>
+        Below are the interview questions with your given answers, their correct answers along with feedback for improvement!
+      </h2>
+      {feedbackList&&feedbackList.map((item, index)=>(
+        <Collapsible key={index} className='mt-5'>
+        <CollapsibleTrigger className='bg-secondary p-3 rounded-lg my-2 text-left flex justify-between w-full'>
+        {item.question}<ChevronsUpDown className='h-5 w-5'/>
+        </CollapsibleTrigger>
+        <CollapsibleContent className='my-3 p-3 border rounded-lg w-full'>
+          <div className='flex flex-col gap-3'>
+            <h2><strong>Rating: {item.rating}</strong></h2>
+            <h2 className='text-sm'><strong className='text-yellow-500'>Your Answer: </strong>{item.userAns}</h2>
+            <h2 className='text-sm'><strong className='text-green-500'>Correct Answer: </strong>{item.correctAns}</h2>
+            <h2 className='text-sm'><strong className='text-blue-500'>Feedback: </strong>{item.feedback}</h2>
+          </div>
+        </CollapsibleContent>
+      </Collapsible>      
+      ))}
+
+      <Button className='mt-5' onClick={()=>router.replace('/dashboard')}>Go to Dashboard</Button>
+    </div>
+  );
 }
 
-export default Feedback
+export default Feedback;
